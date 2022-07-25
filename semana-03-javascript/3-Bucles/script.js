4470cae415cc06736e302dc0bc109c2beb61bdd0
Juanfrancisc <juanfrancisc@hotmail.com>
2022-07-25 13:51:35 +0200

    Ejercico 3 actualizado a al apaartado de 8 a 22

--- semana-03-javascript/3-Bucles/script.js
+++ semana-03-javascript/3-Bucles/script.js
@@ -1,15 +1,33 @@
 "use strict";
 
-let resultado;
+// Desde 0 a 24, siendo 24=0
+console.log("De 0 a 24 horas");
 let cucu="";
-for (let i=0; i<=24; i++){
-    if (i>0 && i<=23){
+for (let i=0; i<=23; i++){
+    if (i > 0){
         cucu=cucu+" cucú";
-    }else if (i===24){
+    } else {
         cucu="";
     }
     console.log(i + cucu);
 }
     
 
+// De 8 a 22 horas
+console.log("");
+console.log("De 8 a 22 horas");
+// El sigueinte for es como crear un string variable que contenga la palabra cucus 7 veces
+let cucus="";
+for (let j=0; j<7 ; j++){
+    cucus=cucus+" cucús";
+}
+
+for (let i=8; i<=22; i++){
+    if (i > 0){
+        cucus=cucus+" cucús";
+    } else {
+        cucu="";
+    }
+    console.log(i + cucus);
+}
 
