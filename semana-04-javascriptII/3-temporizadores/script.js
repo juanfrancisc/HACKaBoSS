33d539faeb5944b5f9b400605c94e1ba528dc3c7
Juanfrancisc <juanfrancisc@hotmail.com>
2022-07-28 19:44:59 +0200

    Actualizacion de los ejercios,  falta el 3 entero y el 4 falta poner los setTimeout

--- semana-04-javascriptII/3-temporizadores/script.js
+++ semana-04-javascriptII/3-temporizadores/script.js
@@ -1,2 +1,58 @@
 "use strict";
 
+/*
+
+function mueveReloj(){ 
+    
+    let momentoActual = new Date()
+    let dia = momentoActual.getDay()
+    let hora = momentoActual.getHours() 
+    let minuto = momentoActual.getMinutes() 
+    let segundo = momentoActual+momentoActual.getSeconds()+5 
+
+    let horaImprimible ="Han pasado "+ dia + " dias, " + hora + " horas, " + minuto + " minutos " + segundo + " segundos"
+
+    console.log(horaImprimible)
+
+
+    //La función se tendrá que llamar así misma para que sea dinámica, 
+    //de esta forma:
+
+    //setTimeout(mueveReloj,1000)
+
+
+    let horaActual2= new Date().setDate(0)
+
+    //horaActual2.setDate()
+    console.log(horaActual2)
+    horaActual2=horaActual2+5
+    console.log(horaActual2)
+}
+
+console.log(mueveReloj())*/
+
+let segundos = 0
+let minutos = 0
+function moverReloj2() {
+
+    segundos += 5;
+  
+    if (segundos == 60) {
+  
+      segundos = 0;
+      minutos += 1;
+    }
+  
+    if (segundos < 10 && minutos < 10) {
+      console.log("0" + minutos + ":" + "0" + segundos);
+    } else if (segundos >= 10 && minutos < 10) {
+        console.log("0" + minutos + ":" + segundos);
+    } else if (segundos < 10 && minutos > 10) {
+        console.log(+minutos + ":" + "0" + segundos);
+    } else {
+        console.log(minutos + ":" + segundos);
+    }
+    setTimeout(moverReloj2,5000)
+  }
+
+  console.log(moverReloj2())
--- semana-04-javascriptII/4-asincronia/script.js
+++ semana-04-javascriptII/4-asincronia/script.js
@@ -3,3 +3,42 @@
 //Promesa de la edad. Devuelve un número del 0 al 100, tardando entre 0 y 1 segundos
 const agePromise = new Promise((resolve, reject) => setTimeout(() => resolve(Math.round(Math.random() * 100)), Math.random() * 1000)) 
 
+//Variable creada a mano para hacer pruebas
+//const agePromise = 17
+
+console.log(agePromise)
+
+/*
+const comprobarAge = new Promise((resolve, reject) => {
+    if (agePromise >= 18){
+        resolve (
+            if (agePromise%2 === 0){
+                console.log("HOla")
+            }
+        )
+    }
+    else if (agePromise < 18) {
+        reject("Menor de edad");
+        }
+});
+
+console.log(comprobarAge)
+
+comprobarAge(agePromise).then(result) => { }
+*/
+
+function comprobarAge(num){
+    return new Promise ((resolve, reject) =>{
+        if(num<18){
+            reject ("Menor de edad")
+        } else{
+            if (num % 2 === 0){
+                resolve ("Es par")
+            }else{
+                resolve ("Es impar")
+            }
+        }
+    })
+}
+
+console.log(comprobarAge(agePromise))
